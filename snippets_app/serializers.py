a4e8590b4ecd95bc3b6763e074ab1fa7d4ce1ca3
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-06-18 16:05:57 +0700

    Creating a Serializer class

---
+++ snippets_app/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from .models import Snippet
+from .models import LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    code = serializers.CharField(style={'base_template': 'textarea.html'})
+    linenos = serializers.BooleanField(required=False)
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default="python")
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+
+    def create(self, validated_data):
+        return Snippet.objects.create(**validated_data)
+
+    def update(self, snippet, validated_data):
+        snippet.title = validated_data.get('title', snippet.title)
+        snippet.code = validated_data.get('code', snippet.code)
+        snippet.linenos = validated_data.get('code', snippet.linenos)
+        snippet.language = validated_data.get('code', snippet.language)
+        snippet.style = validated_data.get('code', snippet.style)
+        snippet.save()
+
+        return snippet
